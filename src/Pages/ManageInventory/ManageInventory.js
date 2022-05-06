7d35c3a59ab085b8ef4f3e4103ca8881f28453e4
thowhidulislam <thowhid.ndc14@gmail.com>
2022-05-07 00:30:57 +0600

    add products button added to inventory page

--- src/Pages/ManageInventory/ManageInventory.js
+++ src/Pages/ManageInventory/ManageInventory.js
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom';
 import Table from '../Table/Table';
 
 const ManageInventory = () => {
     return (
         <div className='container'>
             <Table className='w-100'></Table>
+            <Link to='/addProduct'><button>Add Products</button></Link>
         </div>
     );
 };
